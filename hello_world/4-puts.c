76be01b8b24f13d2f4b22a4f74c64a7c65d4f814
MsekniFiras <msekni.firas@gmail.com>
2023-02-19 03:22:21 -0800

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -9,8 +9,8 @@ int main(void)
 
 {
 
-puts(""Programming is like building a multilingual puzzle\n");
-
-return (0);
+char myStr[]="\"Programming is like building a multilingual puzzle";
+puts(myStr);
+return(0);
 
 }
