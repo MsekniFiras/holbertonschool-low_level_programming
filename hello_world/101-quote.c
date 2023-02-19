4ce0fb8b03bd343b0bf67912b691c4345ae34ca7
MsekniFiras <msekni.firas@gmail.com>
2023-02-19 13:05:23 -0800

    101

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-* main - Pints out a part of a quote to the standard error.
-* Return: 1 (Success).
+* main - Prints out a part of a quote to the standard error.
+* Return: Always 1 (Success).
 */
-int main ()
+int main (void)
 {
 write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 58);
 return (1);
