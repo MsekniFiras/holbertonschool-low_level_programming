0c0e96acf0684fe711a0c965b08654d3b5cb0da4
MsekniFiras <msekni.firas@gmail.com>
2023-03-02 03:35:41 -0800

    1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*main - print_alphabet in lowercase followed by a newline
+*/
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+_putchar(c);
+_putchar('\n');
+}
