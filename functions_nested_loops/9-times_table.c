dd70e75a99b6a9abe5b191fe10035114bb6e55a9
MsekniFiras <msekni.firas@gmail.com>
2023-03-03 07:32:26 -0800

    9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,3 +1,4 @@
+
 #include "main.h"
 
 /**
@@ -11,21 +12,27 @@ void times_table(void)
 int i, n, r = 0;
 for (i = 0; i <= 9; i++)
 {
-	for (n = 1; n <= 9; n++)
+	for (n = 0; n <= 9; n++)
 	{
 		r = i * n;
-		if (r <= 9)
+
+		if (n == 0)
 		{
 			_putchar(r + '0');
+		}
+		else if (r <= 9)
+		{
 			_putchar(',');
 			_putchar(' ');
+			_putchar(' ');
+		        _putchar(r + '0');
 		}
 		else
 		{
-			_putchar(r / 10 + '0');
-			_putchar(r % 10 + '0');
 			_putchar(',');
 			_putchar(' ');
+			_putchar(r / 10 + '0');
+                        _putchar(r % 10 + '0');
 		}
 	}
 	_putchar('\n');
