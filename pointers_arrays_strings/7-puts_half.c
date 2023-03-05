596cf7d404d7ada044ce69707258664cdeda3207
MsekniFiras <msekni.firas@gmail.com>
2023-03-05 14:49:29 -0800

    7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*puts_half - prints half of a string followed by a newline.
+*@str: string.
+*/
+void puts_half(char *str)
+{
+int i = 0, l = 0, n;
+while (str[i++])
+l++;
+if ((l % 2) == 0)
+n = l / 2;
+else
+n = (l + 1) / 2;
+for (i = n; i < l; i++)
+_putchar(str[i]);
+_putchar('\n');
+}
