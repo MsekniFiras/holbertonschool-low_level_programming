1b4a460d8ba5558dc10a06906c03e8a0df4f51c9
MsekniFiras <msekni.firas@gmail.com>
2023-03-05 14:32:40 -0800

    5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*rev_string - reverses a string.
+*@s: string.
+*/
+void rev_string(char *s)
+{
+int l = 0, i = 0;
+char c;
+while (s[i++])
+l++;
+for (i = l - 1; i >= l / 2; i--)
+{
+t = s[i];
+s[i] = s[l - i - 1];
+s[l - i - 1] = c;
+}
+}
