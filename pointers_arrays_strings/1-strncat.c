032d2c40503198562788f9bb2192bb30fb1345c7
MsekniFiras <msekni.firas@gmail.com>
2023-04-05 21:15:03 -0700

    1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -11,7 +11,7 @@ char *_strncat(char *dest, char *src, int n)
 int i = 0, l = 0;
 while (dest[i++])
 l++;
-for (i = 0, src[i] && i < n; i++)
+for (i = 0; src[i] && i < n; i++)
 dest[l] = src[i];
 return (dest);
 }
