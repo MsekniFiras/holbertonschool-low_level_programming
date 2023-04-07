5b5e95f81335c686e5ce5bb3a58b091304e13636
MsekniFiras <msekni.firas@gmail.com>
2023-04-07 15:08:45 -0700

    33

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -3,6 +3,7 @@
 * _strcmp - compares two strings.
 * @s1: first string to be compared.
 * @s2: second string to be compared.
+*Return: difference of the first unmatched characters.
 */
 int _strcmp(char *s1, char *s2)
 {
