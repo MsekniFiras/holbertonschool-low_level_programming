15cab3453d6b31a960c6413a61e066fae8acfafe
MsekniFiras <msekni.firas@gmail.com>
2023-04-08 22:13:25 -0700

    2

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -8,15 +8,11 @@
 */
 int main(int argc, char *argv[])
 {
-int n1, n2, mult;
 if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
-n1 = atoi(argv[1]);
-n2 = atoi(argv[2]);
-mult = n1 *n2;
-printf("%d\n", mult);
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 return (0);
 }
