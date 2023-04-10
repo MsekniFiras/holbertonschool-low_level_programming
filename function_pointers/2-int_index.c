d0f345a8d5fdbf94ead973fb6efd16c668a4fba7
MsekniFiras <msekni.firas@gmail.com>
2023-04-09 21:40:47 -0700

    2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 /**
 * int_index - searches for an integer.
-* array: array of integers.
+* @array: array of integers.
 * @size: size of the array.
 * @cmp: a pointer to the function to be used to compare values.
 * Return: -1 if no match otherwise the index of the first element.
