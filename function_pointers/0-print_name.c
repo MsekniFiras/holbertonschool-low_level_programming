eb4c47333d913d959541a39cecc27aaa3019767c
MsekniFiras <msekni.firas@gmail.com>
2023-04-09 21:23:59 -0700

    0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -4,7 +4,7 @@
 * @name: obviously a name.
 * @f: a pointer to a function.
 */
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *))
 {
 if (name == NULL || f == NULL)
 return;
