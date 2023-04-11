a4cd38a4ba6187129e0b80b0a25773dfd13eef61
MsekniFiras <msekni.firas@gmail.com>
2023-04-10 19:35:58 -0700

    4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+* free_dlistint - frees a linked dlistint_t list.
+* @head: head of the list.
+*/
+void free_dlistint(dlistint_t *head)
+{
+dlistint_t *t
+while (head)
+{
+t = head->next;
+free(head);
+head = t;
+}
+}
